d4257ee00b05cf0016d1fd9dbed2cc556a889cb3
Bekizhan <bekjanabdulzhanov@gmail.com>
2025-06-22 10:32:50 +0600

    update all

--- bpn_teletime/config.py
+++ bpn_teletime/config.py
@@ -8,7 +8,8 @@ WORKTIME_FILE = 'work_time.csv'
 EXCEL_REPORT_DIR = 'work_reports'
 USERS_FILE = 'users.csv'
 
-TIMEZONE = 'Asia/Bishkek'
+
 from dotenv import load_dotenv
 load_dotenv()
 TOKEN = os.getenv("TOKEN")
+TIMEZONE = 'Asia/Bishkek'
--- bpn_teletime/utils.py
+++ bpn_teletime/utils.py
@@ -2,10 +2,13 @@ from datetime import datetime
 import pytz
 from config import TIMEZONE
 
+# Создаем объект временной зоны
 kyrgyz_tz = pytz.timezone(TIMEZONE)
 
 def now():
+    """Текущее время в указанной временной зоне"""
     return datetime.now(kyrgyz_tz)
 
 def format_now():
+    """Отформатированное время"""
     return now().strftime("%Y-%m-%d %H:%M:%S")
