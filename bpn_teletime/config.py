9612977fedef9b16c4bf4330d946614d3f39ba8a
Bekizhan <bekjanabdulzhanov@gmail.com>
2025-06-26 22:45:35 +0600

    Update all

--- bpn_teletime/config.py
+++ bpn_teletime/config.py
@@ -3,9 +3,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-TOKEN = os.getenv("TOKEN")
+TOKEN = os.getenv("TOKEN")  # ← ВАЖНО
 ADMIN_ID = 557174721
-PORT = int(os.getenv("PORT", 8080))
+PORT = int(os.getenv("PORT", 8080))  # если используешь PORT
 
 WORKTIME_FILE = 'work_time.csv'
 EXCEL_REPORT_DIR = 'work_reports'
