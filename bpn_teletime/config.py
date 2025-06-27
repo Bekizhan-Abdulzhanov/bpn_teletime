0931e3e702f307360cb7bbb47705e9159d63f549
Bekizhan <bekjanabdulzhanov@gmail.com>
2025-06-27 10:25:46 +0600

    Update

--- bpn_teletime/config.py
+++ bpn_teletime/config.py
@@ -6,22 +6,18 @@ load_dotenv()
 TOKEN = os.getenv("TOKEN")
 ADMIN_PASSWORD = os.getenv("ADMIN_PASSWORD")
 
-# Приводим PORT к целому числу, обрабатываем ошибку, если невалиден
 try:
     PORT = int(os.getenv("PORT"))
     if not (0 <= PORT <= 65535):
         raise ValueError("PORT must be between 0 and 65535")
-except (TypeError, ValueError):
-    PORT = 8080  # Значение по умолчанию
+except Exception:
+    PORT = 8080  # fallback по умолчанию
 
-
-# Пути к рабочим файлам
 WORKTIME_FILE = 'work_time.csv'
 EXCEL_REPORT_DIR = 'work_reports'
 USERS_FILE = 'users.csv'
 ADMINS_FILE = 'admins.csv'
 
-# Часовой пояс
 TIMEZONE = 'Asia/Bishkek'
 
 
