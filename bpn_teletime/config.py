cfea912e4c343486bcd8dfc7a058974644e342ed
Bekizhan <bekjanabdulzhanov@gmail.com>
2025-06-27 10:43:47 +0600

    Update

--- bpn_teletime/config.py
+++ bpn_teletime/config.py
@@ -4,19 +4,13 @@ from dotenv import load_dotenv
 load_dotenv()
 
 TOKEN = os.getenv("TOKEN")
-ADMIN_PASSWORD = os.getenv("ADMIN_PASSWORD")
-
-try:
-    PORT = int(os.getenv("PORT"))
-    if not (0 <= PORT <= 65535):
-        raise ValueError("PORT must be between 0 and 65535")
-except Exception:
-    PORT = 8080  # fallback по умолчанию
+PORT = int(os.getenv("PORT", 8080))
+ADMIN_PASSWORD = os.getenv("ADMIN_PASSWORD", "BPN123")
+ADMIN_ID = int(os.getenv("ADMIN_ID", 0))  # 👈 ДОБАВЬ ЭТУ СТРОКУ
 
 WORKTIME_FILE = 'work_time.csv'
 EXCEL_REPORT_DIR = 'work_reports'
 USERS_FILE = 'users.csv'
-ADMINS_FILE = 'admins.csv'
 
 TIMEZONE = 'Asia/Bishkek'
 
