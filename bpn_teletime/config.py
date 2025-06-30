d9b6356e0d818a1460f8b15af679f89512f15527
Bekizhan <bekjanabdulzhanov@gmail.com>
2025-06-30 16:12:55 +0600

    Update worktime_file

--- bpn_teletime/config.py
+++ bpn_teletime/config.py
@@ -3,24 +3,19 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# 🔐 Основные переменные
 TOKEN = os.getenv("TOKEN")
 PORT = int(os.getenv("PORT", 8443))
 TIMEZONE = os.getenv("TIMEZONE", "Asia/Bishkek")
 
-# 👮‍♂️ ID админов
 ADMIN_IDS = [
     557174721,  # BekizhanAbdulzhanov
     378268765,  # ErlanNasiev
 ]
 
-# 👑 Автоматически одобренные пользователи
 AUTO_APPROVED_USERS = {
     378268765: "ErlanNasiev",
     557174721: "BekizhanAbdulzhanov",
 }
 
-# 📁 Пути к CSV-файлам (в корне проекта)
-WORKTIME_FILE = "work_time.csv"
-USERS_FILE = "users.csv"
-AUTO_ENABLED_FILE = "auto_enabled.csv"
+WORKTIME_FILE = "worktime.csv"
+
